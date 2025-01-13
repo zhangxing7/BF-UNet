844ea3a41bf6c2c3c06cc5c4d8b83d1ea4c6109f
zhangxing7 <752950156@qq.com>
2025-01-13 10:27:28 +0800

    Update BF-UNet.py

--- BF-UNet.py
+++ BF-UNet.py
@@ -54,9 +54,9 @@ def act_layer(act, inplace=False, neg_slope=0.2, n_prelu=1):
         raise NotImplementedError('activation layer [%s] is not found' % act)
     return layer
 
-class LGAG(nn.Module):
+class LAG(nn.Module):
     def __init__(self, F_g, F_l, F_int, kernel_size=3, groups=1, activation='relu'):
-        super(LGAG, self).__init__()
+        super(LAG, self).__init__()
 
         if kernel_size == 1:
             groups = 1
@@ -105,7 +105,7 @@ class BasicConv2d(nn.Module):
         x = self.bn(x)
         return x
 
-class EAblock(nn.Module):
+class RRM(nn.Module):
     def __init__(self, in_c):
         super().__init__()
 
@@ -141,24 +141,13 @@ class EAblock(nn.Module):
 
 class DNM_Conv(nn.Module):
     def __init__(self, input_size, out_size, M, activation=F.relu):
-        super(DNM_Conv, self).__init__()#随机权重矩阵DNM_W，形状为[out_size, M, input_size]
-        DNM_W = torch.rand([out_size, M, input_size])  # .cuda() # [size_out, M, size_in]  [num_class, M, 512 * 3 * 3]
+        super(DNM_Conv, self).__init__()#
+        DNM_W = torch.rand([out_size, M, input_size])  #
         DNM_q = torch.rand([out_size, M, input_size])
-        # dendritic_W = torch.rand([input_size])  # .cuda() # size_out, M, size_in]
-        # membrane_W = torch.rand([M])  # .cuda() # size_out, M, size_in]
-        # q = torch.rand([out_size, M, input_size])  # .cuda()
-        # torch.nn.init.constant_(q, qs)  # 设置q的初始值
-        # torch.nn.init.normal_(dendritic_W, mean=0,std=1)
-        # torch.nn.init.normal_(dendritic_W, mean=0,std=1)
-        # torch.nn.init.normal_(dendritic_W, mean=0,std=1)
-        # k = torch.tensor(k).to(DEVICE)
         qs = torch.rand(1)
         qs = torch.tensor(qs).to(DEVICE)
         self.params = nn.ParameterDict({'DNM_W': nn.Parameter(DNM_W)})
         self.params.update({'q': nn.Parameter(DNM_q)})
-        # self.params.update({'dendritic_W': nn.Parameter(dendritic_W)})
-        # self.params.update({'membrane_W': nn.Parameter(membrane_W)})
-        # self.k = k
         self.qs = qs
         self.activation = activation
 
@@ -166,40 +155,20 @@ class DNM_Conv(nn.Module):
         self.norm2 = nn.LayerNorm(input_size)
 
     def forward(self, x):
-        # Synapse 突触 synaptic layer possesses a sigmoid function
         out_size, M, _ = self.params['DNM_W'].shape
-        # pdb.set_trace()
-        # x.shape (batch*64*256*256)    (0,1,2,3) ==> (0,2,3,1) => (0,1,2,3) (permute(0,3,1,2))
-        x = torch.permute(x, (0, 2, 3, 1))  # torch.Size([8, 256, 256, 64])
-        x = torch.unsqueeze(x, 3) # 扩展维度
-        # print("x shape 240:",x.shape) #([12, 112, 112, 1, 32])
-        x = torch.unsqueeze(x, 4) # 扩展维度
-        # print("x shape 242:", x.shape)#([12, 112, 112, 1, 1, 32])
+        x = torch.permute(x, (0, 2, 3, 1))  #
+        x = torch.unsqueeze(x, 3) #
+        x = torch.unsqueeze(x, 4) #
         x = self.norm1(x)  # norm
 
-        x = x.repeat(1, 1, 1, out_size, M, 1) #在新扩展的维度上复制
-        # print("x shape 246:", x.shape) #([12, 112, 112, 1, 10, 32])
-        # x = F.relu(torch.mul(self.k, (torch.mul(x, self.params['DNM_W']) - self.params['q'])))
-        x = F.relu(torch.mul(x, self.params['DNM_W']) - self.params['q'])#输入x的第i个元素的第j个树突状分支的输出。该操作涉及权重与输入x之间的逐元素乘法，然后减去阈值
-        # x = torch.mul(x, self.params['DNM_W'])  # DNM_W = torch.rand([out_size, M, input_size]) [1, 10, 32]
-        # x = F.relu(self.k * (x - self.params['q']))
+        x = x.repeat(1, 1, 1, out_size, M, 1) #
+        x = F.relu(torch.mul(x, self.params['DNM_W']) - self.params['q'])
 
-        # Dendritic 树突  Its dendrite layer has a multiplicative function
         x = self.norm2(x)  # norm、
 
-        # x = torch.mul(x, self.params['dendritic_W'])
-        # x = x * self.params['dendritic_W']
-        x = torch.sum(x, 5) #([12, 112, 112, 5, 5])
-        # print("x shape 258:", x.shape)
-        # x = torch.sigmoid(x)
-        # x = F.relu(x)
-        # pdb.set_trace()
-        # Membrane 膜层
-        # x = torch.mul(x, self.params['membrane_W'])
-        # x = x * self.params['membrane_W']
+        x = torch.sum(x, 5)
         x = torch.sum(x, 4)
-        # print("x shape 266:", x.shape)  #([12, 112, 112, 1])
-        x = torch.permute(x, (0, 3, 1, 2)) #([12, 1, 112, 112])
+        x = torch.permute(x, (0, 3, 1, 2))
 
         # Soma 胞体层
         if self.activation != None:
@@ -220,7 +189,7 @@ class conv_dnm(nn.Module):
 
 class BFUNet(nn.Module):
     
-    def __init__(self, mamba,num_classes=1, input_channels=3, c_list=[8,16,24,32,48,64], bridge=True, gt_ds=True):
+    def __init__(self, num_classes=1, input_channels=3, c_list=[8,16,24,32,48,64], bridge=True, gt_ds=True):
         super().__init__()
         self.c_list = c_list
         self.bridge = bridge
@@ -263,10 +232,10 @@ class BFUNet(nn.Module):
 
         self.resnet = res2net50_v1b_26w_4s(pretrained=True)
 
-        lgag_ks = 3
-        self.lgag4 = LGAG(F_g= c_list[3], F_l= c_list[3], F_int= c_list[3]//2, kernel_size=lgag_ks, groups= c_list[3]//2, activation='relu')
-        self.lgag3 = LGAG(F_g= c_list[2], F_l= c_list[2], F_int= c_list[2]//2, kernel_size=lgag_ks, groups= c_list[2]//2, activation='relu')
-        self.lgag2 = LGAG(F_g= c_list[1], F_l= c_list[1], F_int= c_list[1]//2, kernel_size=lgag_ks, groups= c_list[1]//2, activation='relu')
+        LAG_ks = 3
+        self.LAG4 = LAG(F_g= c_list[3], F_l= c_list[3], F_int= c_list[3]//2, kernel_size=LAG_ks, groups= c_list[3]//2, activation='relu')
+        self.LAG3 = LAG(F_g= c_list[2], F_l= c_list[2], F_int= c_list[2]//2, kernel_size=LAG_ks, groups= c_list[2]//2, activation='relu')
+        self.LAG2 = LAG(F_g= c_list[1], F_l= c_list[1], F_int= c_list[1]//2, kernel_size=LAG_ks, groups= c_list[1]//2, activation='relu')
 
         channel = 32
         self.Translayer_1 = BasicConv2d(256, c_list[1], 1)
@@ -292,14 +261,14 @@ class BFUNet(nn.Module):
 
         self.norm_up = norm_layer(64)
 
-        self.EA1 = EAblock(c_list[1])
-        self.EA2 = EAblock(c_list[2])
-        self.EA3 = EAblock(c_list[3])
-        self.EA4 = EAblock(c_list[4])
+        self.rrm1 = RRM(c_list[1])
+        self.rrm2 = RRM(c_list[2])
+        self.rrm3 = RRM(c_list[3])
+        self.rrm4 = RRM(c_list[4])
 
-        self.EA5 = EAblock(c_list[3])
-        self.EA6 = EAblock(c_list[2])
-        self.EA7 = EAblock(c_list[1])
+        self.rrm5 = RRM(c_list[3])
+        self.rrm6 = RRM(c_list[2])
+        self.rrm7 = RRM(c_list[1])
 
 
         self.Dnm_conv2d = conv_dnm(c_list[0], 1, 10)
@@ -324,16 +293,16 @@ class BFUNet(nn.Module):
         t_f1,t_f2,t_f3,t_f4 = self.resnet(x)
 
         f1 = self.Translayer_1(t_f1)
-        f1 = self.EA1(f1)
+        f1 = self.rrm1(f1)
 
         f2 = self.Translayer_2(t_f2)
-        f2 = self.EA2(f2)
+        f2 = self.rrm2(f2)
 
-        f3 = self.Translayer_3(t_f3)
-        f3 = self.EA3(f3)
+        f3 = self.Translayer_3(t_f3) 
+        f3 = self.rrm3(f3)
 
         f4 = self.Translayer_4(t_f4)
-        f4 = self.EA4(f4)
+        f4 = self.rrm4(f4)
 
         t2=f1
         t3=f2
@@ -342,31 +311,31 @@ class BFUNet(nn.Module):
 
         #----------------------------decoder--------------------------------------------------------------------
         out5 = t5
-        out4 = F.gelu(self.dbn2(self.decoder2(out5)))
-        out4 = self.EA5(out4)
+        out4 = F.gelu(self.dbn2(self.decoder2(out5)))  
+        out4 = self.rrm5(out4)
 
-        t4 = self.lgag4(g=out4, x=t4)
+        t4 = self.LAG4(g=out4, x=t4)
         if self.gt_ds:
             gt_pre4 = self.gt_conv2(out4)
             gt_pre4 = F.interpolate(gt_pre4, scale_factor=16, mode ='bilinear', align_corners=True)
-        out4 = torch.add(out4, t4)
+        out4 = torch.add(out4, t4) 
 
         out3 = F.gelu(self.dbn3(self.decoder3(out4)))
-        out3 = self.EA6(out3)
-        t3 = self.lgag3(g=out3, x=t3)
+        out3 = self.rrm6(out3)
+        t3 = self.LAG3(g=out3, x=t3)
         if self.gt_ds:
             gt_pre3 = self.gt_conv3(out3)
             gt_pre3 = F.interpolate(gt_pre3, scale_factor=8, mode ='bilinear', align_corners=True)
-        out3 = torch.add(out3, t3)
+        out3 = torch.add(out3, t3) 
 
 
         out2 = F.gelu(self.dbn4(self.decoder4(out3)))
-        out2 = self.EA7(out2)
-        t2 = self.lgag2(g=out2, x=t2)
+        out2 = self.rrm7(out2)
+        t2 = self.LAG2(g=out2, x=t2)
         if self.gt_ds:
             gt_pre2 = self.gt_conv4(out2)
             gt_pre2 = F.interpolate(gt_pre2, scale_factor=4, mode ='bilinear', align_corners=True)
-        out2 = torch.add(out2, t2)
+        out2 = torch.add(out2, t2) 
         out1 = F.gelu(F.interpolate(self.dbn5(self.decoder5(out2)), scale_factor=(2, 2), mode='bilinear',align_corners=True))  # b, c0, H/2, W/2
 
         gt_pre1 = self.gt_conv5(out1)
